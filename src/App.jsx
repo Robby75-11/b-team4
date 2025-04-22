7b01262a02b50676311d30041b326173cc5567cb
annafirinu <anna.firinu2@outlook.it>
2025-04-22 15:05:00 +0200

    MainProfile

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import "./App.css";
 import NavBar from "./assets/components/NavBar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import MainProfile from "./component/MainProfile";
 function App() {
   return (
     <>
       <NavBar style={{ position: "sticky", top: 0, zIndex: 1000 }} />
+      <MainProfile />
     </>
   );
 }
