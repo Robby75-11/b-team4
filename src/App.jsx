fb1800063a9b3ed7c3581052ae70e69e3dd74cde
annafirinu <anna.firinu2@outlook.it>
2025-04-22 16:32:53 +0200

    Lingue

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import MainProfile from "./component/MainProfile";
 import Formazione from "./component/Formazione";
+import Lingue from "./component/Lingue";
 function App() {
   return (
     <>
       <NavBar style={{ position: "sticky", top: 0, zIndex: 1000 }} />
       <MainProfile />
       <Formazione />
+      <Lingue />
     </>
   );
 }
