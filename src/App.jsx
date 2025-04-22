927fce0d61fc06eeadc568056c2aff268974f1c6
Marco Teodoro Mancuso <marco.teo.mancuso@gmail.com>
2025-04-22 12:23:50 +0200

    first push + NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
-
+import NavBar from "./assets/components/NavBar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 function App() {
-  return <></>;
+  return (
+    <>
+      <NavBar style={{ position: "sticky", top: 0, zIndex: 1000 }} />
+    </>
+  );
 }
 
 export default App;
