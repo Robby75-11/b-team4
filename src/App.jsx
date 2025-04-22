54f6280f4437f82c731baf2bc9fbf085aa43f1f1
Roberto Albergo <ralbergo7@gmail.com>
2025-04-22 17:31:35 +0200

    g

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import SideBar from "./component/SideBar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <div className="app-layout">
+        <div className="main-content">
+          <Routes></Routes>
+        </div>
+
+        {/* Sidebar a destra */}
+        <div className="sider-area ">
+          <SideBar />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
