50ec9bbaecf6e7d26095691c7b1e9a76e1a1c49d
annafirinu <anna.firinu2@outlook.it>
2025-04-23 11:17:50 +0200

    modifica-grafica-esperienze

--- src/component/Esperienze.jsx
+++ src/component/Esperienze.jsx
@@ -32,7 +32,63 @@ const Esperienze = () => {
   }, [dispatch]);
 
   return (
-    <Container
+    <div
+      className="border border-2 rounded-2 p-3"
+      style={{ backgroundColor: "#FFFFFF", borderColor: "#D2D2D2" }}
+    >
+      <div className="d-flex justify-content-between align-items-center">
+        <h1>Esperienze</h1>
+        <div>
+          <Row className="d-flex align-items-center gap-2">
+            <Col>
+              <i
+                className="bi bi-plus-lg text-black"
+                onClick={() => console.log("Plus icon clicked")}
+                style={{ cursor: "pointer" }}
+              ></i>
+            </Col>
+            <Col>
+              <i
+                className="bi bi-pencil text-black"
+                onClick={() => console.log("Pencil icon clicked")}
+                style={{ cursor: "pointer" }}
+              ></i>
+            </Col>
+          </Row>
+        </div>
+      </div>
+      <div className="d-flex border-bottom border-secondary">
+        <div>
+          <img
+            src={profile.image || "placeholder.jpg"}
+            alt="Company Logo"
+            style={{ height: "80px", width: "80px" }}
+            className="me-3 mt-3"
+          />
+        </div>
+        <div className="mt-3 lh-1">
+          <p className="fw-bold">
+            {" "}
+            {profile.title || "Titolo non disponibile"}
+          </p>
+          <p>Azienda</p>
+          <p className="text-secondary">
+            {new Date(profile.createdAt).toLocaleDateString() ||
+              "Data creazione non disponibile"}{" "}
+            -{" "}
+            {new Date(profile.updatedAt).toLocaleDateString() ||
+              "Data aggiornamento non disponibile"}
+          </p>
+          <div className="fw-bold mt-5 mb-4">
+            {profile.area || "Area non disponibile"}
+          </div>
+        </div>
+      </div>
+    </div>
+
+    /* Ho lasciato commentat la vecchia struttura, qualora qualcuno ci abbia lavorato 
+prima della modifica e avesse bisogno di un riscontro*/
+    /* <Container
       fluid
       style={{ backgroundColor: "#FFFFFF", borderColor: "#D2D2D2" }}
     >
@@ -83,7 +139,7 @@ const Esperienze = () => {
           </Row>
         </Col>
       </Row>
-    </Container>
+    </Container>*/
   );
 };
 
