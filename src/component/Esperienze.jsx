245fc9ad6de9c202839fe8555e8717fb1ebef0b3
Roberto Albergo <ralbergo7@gmail.com>
2025-04-23 22:11:07 +0200

    Update Esperienze.jsx

--- src/component/Esperienze.jsx
+++ src/component/Esperienze.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Row, Col, Container, Modal, Button, Form } from "react-bootstrap";
+import { Row, Col, Modal, Button, Form } from "react-bootstrap";
 import { useSelector } from "react-redux";
 
 const URL =
